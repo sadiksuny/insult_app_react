730651ac753ff2baec37e1db87aafe16a525b54d
Sadik Suny <sadik.m.sunny@gmail.com>
2020-03-22 19:26:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state ={
+        insult :''
+    };
+    componentDidMount(){
+        this.fetchInsult();
+    }
+    fetchInsult= () => {
+        axios.get("https://cors-anywhere.herokuapp.com/https://insult.mattbas.org/api/insult.json")
+        .then((response)=>{
+            const insult = response.data.insult;
+            this.setState({insult});
+        })
+        .catch((error)=>{
+           console.log(error); 
+        });
+    }
+    render(){
+        const {insult}= this.state;
+        return(
+            
+            <div className="app">
+                <h1> Insult Generator </h1>
+                <h1 className="head">{insult} </h1>
+                <Button variant="contained" onClick={this.fetchInsult}>New Insult</Button>
+                </div>
+        );
+    }
 }
-
 export default App;
